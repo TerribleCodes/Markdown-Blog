37291ec197240f47767f5dd33c4a7b7cd31b23cd
TerribleCodes <madawadesilva9998@gmail.com>
2023-03-04 19:00:00 +0530

    database connection

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+// Route Handling
 const articleRouter = require("./routes/articles");
+app.use(express.urlencoded({ extended: false }));
+app.use("/articles", articleRouter);
 
 // ejs acts as the view engine
 app.set("view engine", "ejs");
-app.use("/articles", articleRouter);
 
 app.get("/", (req, res) => {
   const articles = [
@@ -19,7 +26,15 @@ app.get("/", (req, res) => {
       description: "Test Description2",
     },
   ];
+
+  // render method will automatically look into the views folder and therefore no need to set the path with `views`
   res.render("articles/index", { articles: articles });
 });
 
-app.listen(5000);
+mongoose.connect(process.env.MONGO_URL).then(
+  () => {
+    console.log("Listening on port 5000");
+    app.listen(5000);
+  },
+  (err) => console.log(err)
+);
